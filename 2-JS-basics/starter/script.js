c18de6dd80f04e8386304cf7cf823dafa70cd9de
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-02-13 12:02:17 -0600

    Notes and examples about type coercion and variable mutation.
    In JavaScript, variables can be coerced into other data types when variables of different types are combined in expressions. You can also redefine variables as other data types.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,6 @@
+/********* Variables and Data Types ************/
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -32,3 +35,49 @@ console.log($3years);
 // variable names can't be reserved words either
 // var function = 'Not legal';
 // var delete = 22;
+*/
+
+
+/******************** Variable Mutation and Type Coercion ******************/
+
+var firstName = 'John';
+var age = 28;
+
+// implicit type coercion occurs here, as the variable 'age', which is a number data type, is converted to the string data type.
+console.log(firstName + ' ' + age);
+
+// can declare multiple variables together, here they are undefined because we haven't assigned anything to them.
+var job, isMarried;
+
+// now, the variable 'job' is a string data type, and the variable isMarried is a boolean data type.
+job = 'teacher';
+isMarried = false;
+
+// here, the age is converted to a string and the boolean 'isMarried' is also converted to the string 'false'.
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); // output: John is a 28 year old teacher. Is he married? false
+
+// if we make isMarried undefined...
+isMarried = undefined;
+
+// then we see from the output that the string 'undefined' is printed, showing that the type coercion from undefined to string occurs as well.
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried); // output: John is a 28 year old teacher. Is he married? undefined
+
+// because JavaScript is dynamically typed, we can assign a different data type to a variable, as we do with 'age' below. It used to be a number, now it is a string.
+age = 'twenty eight';
+
+// instead of logging to the console, you can also use the alert() function to cause a popup to open.
+alert('Hi there!');
+
+// also, if you want to prompt the user for some input, you can use the prompt() function and store the value returned in a variable.
+var lastName = prompt('What is your last name?');
+console.log('Your last name is ' + lastName);
+
+
+
+
+
+
+
+
+
+
