b459c9a1b343d6c4fb49bade2bcea0e0311c6cc2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-02-13 01:44:48 -0600

    Add notes and examples on variables and data types.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,34 @@
-console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+// string data type
+var lastName = 'Smith';
+// number data type
+var age = 28;
+// boolean data type
+var adult = true;
+console.log(adult);
+
+// if we don't assign anything to a variable, it will have a data type of 'undefined.'
+var job;
+console.log(job);
+
+// here, we assign a String to job, so now it has a String data type.
+job = 'Teacher';
+console.log(job);
+
+// can't start a variable with a number, will throw a syntax error!
+// var 3years = 3;
+
+// _ and $ are fine though
+var _3years = 3;
+var $3years = 3;
+console.log(_3years);
+console.log($3years);
+
+// forbidden symbols can't be in the middle of variables either 
+// var john/mark = "john and mark";
+
+// variable names can't be reserved words either
+// var function = 'Not legal';
+// var delete = 22;
